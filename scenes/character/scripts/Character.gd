a31541c7c6a9d20ce116c620e9ef400fbfd5b1f0
Alexander Stone <aloecend@gmail.com>
2018-10-31 11:54:25 -0700

    Pushing script changes.

--- scenes/character/scripts/Character.gd
+++ scenes/character/scripts/Character.gd
@@ -1,15 +1,31 @@
 extends Area2D
+signal hit
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+export (int) var speed
+var screensize
 
 func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
-	pass
+	screensize = get_viewport_rect().size
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
+func _physics_process(delta):
+	#Change the below to not use ui input at some point
+	#TODO: Change this later
+	var velocity = Vector2()
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+	position += velocity
+	position.x = clamp(position.x, 0, screensize.x)
+	position.y = clamp(position.y, 0, screensize.y)
+
+func _on_Character_body_entered(body):
+	#TODO: Stop movement on hit
+	emit_signal("hit")
+	

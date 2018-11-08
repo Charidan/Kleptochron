50bfcbdbc0bcea687cccd95c2cf351b37c39004f
Richard Nicholson <twilly0@gmail.com>
2018-11-08 14:32:36 -0800

    Fixed up the HUD. Buttons and the slider can no longer steal UI input.

--- scripts/time_slider.gd
+++ scripts/time_slider.gd
@@ -10,6 +10,7 @@ const UPP_DISTANCE = 600
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
+	self.focus_mode = Control.FOCUS_NONE
 	self.connect("value_changed", self, "timeskip")
 
 func timeskip(val):
@@ -17,4 +18,6 @@ func timeskip(val):
 	emit_signal("pause")
 	# TODO ask global to travel us back to the time
 	print("UPP = " + str(global.furthest_present - UPP_DISTANCE) + " beach = " + str(global.furthest_present) + " target = " + str(lerp(global.furthest_present - UPP_DISTANCE, global.furthest_present, val)))
-	global.time_travel_back(lerp(global.furthest_present - UPP_DISTANCE, global.furthest_present, val), get_tree().get_root().get_children()[len(get_tree().get_root().get_children()) - 1].get_children())
+	#lerp between furthest_present anchor and UPP to get target time
+	#subtract target time from current time to get delta
+	global.time_travel_back(global.time - lerp(global.furthest_present - UPP_DISTANCE, global.furthest_present, val), get_tree().get_root().get_children()[len(get_tree().get_root().get_children()) - 1].get_children())
